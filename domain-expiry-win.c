57b0362ad2df924adade0659f3d7ff48e103ce92
OriginalEggy <56088013+OriginalEggy@users.noreply.github.com>
2023-05-22 09:53:20 -0400

    Update domain-expiry-win.c
    Removed text from final printf so it only returns the value in daysToExpiration before exiting.

--- domain-expiry-win.c
+++ domain-expiry-win.c
@@ -70,7 +70,7 @@ int main(int argc, char* argv[]) {
     int daysToExpiration = calculateDaysToExpiration(dateStart);
 
     // Print the result
-    printf("Days to expiration: %d\n", daysToExpiration);
+    printf("%d\n", daysToExpiration);
 
     return 0;
 }
